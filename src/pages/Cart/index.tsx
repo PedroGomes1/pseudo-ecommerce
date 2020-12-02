af6726a75b440238d77fc73fc53fb9f170c5f643
Pedro <pedro-gomes17@hotmail.com>
2020-12-02 10:43:46 -0300

    Cart page

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,13 +1,223 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {
+  Container,
+  Card,
+  ButtonDeleteProduct,
+  ProductImage,
+  ProductInfos,
+  Description,
+  ProductPrice,
+  ActionQuantity,
+  ProductQuantity,
+  ButtonIncrement,
+  ButtonDecrement,
+  ProductPriceFinal,
+  TotalPaylable,
+  InfoTotalPrice,
+  TextTotalPrice,
+  TextValue,
+  ButtonFinalizePayment,
+  TextButtonFinalizePayment,
+} from './styles';
 
 const Cart: React.FC = () => {
   return (
-    <View>
-      <Text>Hello Cart</Text>
-    </View>
+    <>
+      <Container showsVerticalScrollIndicator={false}>
+        <Card>
+          <ButtonDeleteProduct>
+            <Icon name="close" size={15} color="#000000" />
+          </ButtonDeleteProduct>
+
+          <ProductImage
+            source={{
+              uri:
+                'https://storage.googleapis.com/golden-wind/bootcamp-gostack/camiseta-ecommerce.jpg',
+            }}
+          />
+
+          <ProductInfos>
+            <Description>Cadeira louca do Pedro asdf asdf</Description>
+
+            <ProductPrice>R$ 300,00</ProductPrice>
+
+            <ActionQuantity>
+              <ButtonIncrement>
+                <Icon name="add" size={15} color="#fff" />
+              </ButtonIncrement>
+              <ProductQuantity>3</ProductQuantity>
+              <ButtonDecrement>
+                <Icon name="remove" size={15} color="#fff" />
+              </ButtonDecrement>
+              <ProductPriceFinal>R$1.5000,99</ProductPriceFinal>
+            </ActionQuantity>
+          </ProductInfos>
+        </Card>
+
+        <Card>
+          <ButtonDeleteProduct>
+            <Icon name="close" size={15} color="#000000" />
+          </ButtonDeleteProduct>
+
+          <ProductImage
+            source={{
+              uri:
+                'https://storage.googleapis.com/golden-wind/bootcamp-gostack/camiseta-ecommerce.jpg',
+            }}
+          />
+
+          <ProductInfos>
+            <Description>Cadeira louca do Pedro asdf asdf</Description>
+
+            <ProductPrice>R$ 300,00</ProductPrice>
+
+            <ActionQuantity>
+              <ButtonIncrement>
+                <Icon name="add" size={15} color="#fff" />
+              </ButtonIncrement>
+              <ProductQuantity>3</ProductQuantity>
+              <ButtonDecrement>
+                <Icon name="remove" size={15} color="#fff" />
+              </ButtonDecrement>
+              <ProductPriceFinal>R$1.5000,99</ProductPriceFinal>
+            </ActionQuantity>
+          </ProductInfos>
+        </Card>
+
+        <Card>
+          <ButtonDeleteProduct>
+            <Icon name="close" size={15} color="#000000" />
+          </ButtonDeleteProduct>
+
+          <ProductImage
+            source={{
+              uri:
+                'https://storage.googleapis.com/golden-wind/bootcamp-gostack/camiseta-ecommerce.jpg',
+            }}
+          />
+
+          <ProductInfos>
+            <Description>Cadeira louca do Pedro asdf asdf</Description>
+
+            <ProductPrice>R$ 300,00</ProductPrice>
+
+            <ActionQuantity>
+              <ButtonIncrement>
+                <Icon name="add" size={15} color="#fff" />
+              </ButtonIncrement>
+              <ProductQuantity>3</ProductQuantity>
+              <ButtonDecrement>
+                <Icon name="remove" size={15} color="#fff" />
+              </ButtonDecrement>
+              <ProductPriceFinal>R$1.5000,99</ProductPriceFinal>
+            </ActionQuantity>
+          </ProductInfos>
+        </Card>
+
+        <Card>
+          <ButtonDeleteProduct>
+            <Icon name="close" size={15} color="#000000" />
+          </ButtonDeleteProduct>
+
+          <ProductImage
+            source={{
+              uri:
+                'https://storage.googleapis.com/golden-wind/bootcamp-gostack/camiseta-ecommerce.jpg',
+            }}
+          />
+
+          <ProductInfos>
+            <Description>Cadeira louca do Pedro asdf asdf</Description>
+
+            <ProductPrice>R$ 300,00</ProductPrice>
+
+            <ActionQuantity>
+              <ButtonIncrement>
+                <Icon name="add" size={15} color="#fff" />
+              </ButtonIncrement>
+              <ProductQuantity>3</ProductQuantity>
+              <ButtonDecrement>
+                <Icon name="remove" size={15} color="#fff" />
+              </ButtonDecrement>
+              <ProductPriceFinal>R$159,99</ProductPriceFinal>
+            </ActionQuantity>
+          </ProductInfos>
+        </Card>
+
+        <Card>
+          <ButtonDeleteProduct>
+            <Icon name="close" size={15} color="#000000" />
+          </ButtonDeleteProduct>
+
+          <ProductImage
+            source={{
+              uri:
+                'https://storage.googleapis.com/golden-wind/bootcamp-gostack/camiseta-ecommerce.jpg',
+            }}
+          />
+
+          <ProductInfos>
+            <Description>Cadeira louca do Pedro asdf asdf</Description>
+
+            <ProductPrice>R$ 300,00</ProductPrice>
+
+            <ActionQuantity>
+              <ButtonIncrement>
+                <Icon name="add" size={15} color="#fff" />
+              </ButtonIncrement>
+              <ProductQuantity>3</ProductQuantity>
+              <ButtonDecrement>
+                <Icon name="remove" size={15} color="#fff" />
+              </ButtonDecrement>
+              <ProductPriceFinal>R$150,99</ProductPriceFinal>
+            </ActionQuantity>
+          </ProductInfos>
+        </Card>
+
+        <Card>
+          <ButtonDeleteProduct>
+            <Icon name="close" size={15} color="#000000" />
+          </ButtonDeleteProduct>
+
+          <ProductImage
+            source={{
+              uri:
+                'https://storage.googleapis.com/golden-wind/bootcamp-gostack/camiseta-ecommerce.jpg',
+            }}
+          />
+
+          <ProductInfos>
+            <Description>Cadeira louca do Pedro asdf asdf</Description>
+
+            <ProductPrice>R$ 300,00</ProductPrice>
+
+            <ActionQuantity>
+              <ButtonIncrement>
+                <Icon name="add" size={15} color="#fff" />
+              </ButtonIncrement>
+              <ProductQuantity>3</ProductQuantity>
+              <ButtonDecrement>
+                <Icon name="remove" size={15} color="#fff" />
+              </ButtonDecrement>
+              <ProductPriceFinal>R$30,99</ProductPriceFinal>
+            </ActionQuantity>
+          </ProductInfos>
+        </Card>
+      </Container>
+
+      <TotalPaylable>
+        <InfoTotalPrice>
+          <TextTotalPrice>Preço total:</TextTotalPrice>
+          <TextValue>R$ 250,00</TextValue>
+        </InfoTotalPrice>
+
+        <ButtonFinalizePayment>
+          <TextButtonFinalizePayment>Finalizar</TextButtonFinalizePayment>
+          <Icon name="arrow-forward" size={20} color="#fff" />
+        </ButtonFinalizePayment>
+      </TotalPaylable>
+    </>
   );
 };
 
--- src/pages/Cart/styles.ts
+++ src/pages/Cart/styles.ts
@@ -1,3 +1,134 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
-export const Container = styled.View``;
+export const Container = styled.ScrollView`
+  flex: 1;
+  background-color: #f9fbf2;
+`;
+
+export const Card = styled.View`
+  margin: 15px 25px;
+  flex-direction: row;
+  align-items: center;
+  background-color: #ffffff;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.55);
+  elevation: 5;
+  border-radius: 10px;
+  padding: 15px 10px;
+`;
+
+export const ButtonDeleteProduct = styled(RectButton)`
+  position: absolute;
+  background: #e9e8ec;
+  border-radius: 5px;
+  top: 0px;
+  right: 0px;
+`;
+
+export const ProductImage = styled.Image`
+  height: 92px;
+  width: 92px;
+`;
+
+export const ProductInfos = styled.View`
+  margin-left: 16px;
+  flex: 1;
+`;
+
+export const Description = styled.Text`
+  font-size: 14px;
+  font-family: 'Poppins-Medium';
+`;
+
+export const ProductPrice = styled.Text`
+  font-size: 14px;
+  color: #a0a0b3;
+  margin-top: 8px;
+  font-family: 'Roboto-Regular';
+`;
+
+// export const ProductTotal = styled.Text`
+//   font-size: 18px;
+//   color: #0acf83;
+//   margin-top: 8px;
+//   font-family: 'Roboto-Bold';
+// `;
+
+export const ActionQuantity = styled.View`
+  flex-direction: row;
+  align-items: center;
+
+  margin-top: 8px;
+`;
+
+export const ProductQuantity = styled.Text`
+  margin: 0px 10px;
+  font-family: 'Roboto-Regular';
+`;
+
+export const ButtonIncrement = styled(RectButton)`
+  width: 25px;
+  height: 25px;
+  background: #0acf83;
+  border-radius: 5px;
+
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ButtonDecrement = styled(RectButton)`
+  justify-content: center;
+  align-items: center;
+
+  width: 25px;
+  height: 25px;
+  background: #0acf83;
+  border-radius: 5px;
+`;
+
+export const ProductPriceFinal = styled.Text`
+  margin-left: auto;
+  font-family: 'Roboto-Bold';
+  font-size: 16px;
+`;
+
+export const TotalPaylable = styled.View`
+  border-top-color: rgba(0, 0, 0, 0.2);
+  border-top-width: 2px;
+
+  background: rgba(255, 255, 255, 0.5);
+  padding: 15px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: auto;
+`;
+
+export const InfoTotalPrice = styled.View``;
+
+export const TextTotalPrice = styled.Text`
+  font-size: 12px;
+  font-family: 'Poppins-Medium';
+`;
+
+export const TextValue = styled.Text`
+  font-size: 20px;
+  font-family: 'Roboto-Bold';
+`;
+
+export const ButtonFinalizePayment = styled(RectButton)`
+  width: 140px;
+  height: 40px;
+  border-radius: 50px;
+  background: #0acf83;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+export const TextButtonFinalizePayment = styled.Text`
+  color: #fff;
+  font-family: 'Poppins-Medium';
+`;
