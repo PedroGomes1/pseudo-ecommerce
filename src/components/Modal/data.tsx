30e07890e1fe0263e36163a14c652584b3245451
Pedro <pedro-gomes17@hotmail.com>
2020-12-05 11:57:42 -0300

    Evaluation card data to modal filter

---
+++ src/components/Modal/data.tsx
@@ -0,0 +1,19 @@
+const dataCardsScore = [
+  {
+    id: 1,
+    text: '0-50',
+  },
+  {
+    id: 2,
+    text: '50-100',
+  },
+  {
+    id: 3,
+    text: '100-500',
+  },
+  {
+    id: 4,
+    text: '500-3000',
+  },
+];
+export default dataCardsScore;
